05d22830f35fa2116921ce280d526ccce1237235
NohChanWoo <hlnoh1017.cwn@gmail.com>
2022-06-08 18:41:36 +0900

    4.0

---
+++ src/fbase.js
@@ -0,0 +1,19 @@
+import {initializeApp} from 'firebase/app';
+import {getAuth}from "firebase/auth"
+/*import 'firebase/compat/firestore';*/
+
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+
+  };
+const firebaseapp = initializeApp(firebaseConfig);
+
+export const firebaseInstance =  getAuth();
+export const authService = getAuth(firebaseapp);
